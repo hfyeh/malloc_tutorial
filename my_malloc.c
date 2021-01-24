c462eba346a7b541b35710c403137f39f17d282d
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2021-01-24 16:55:36 +0800

    Add function 'find_block' to find qualified block

--- my_malloc.c
+++ my_malloc.c
@@ -16,6 +16,20 @@ struct block {
 #define align4(x) ((((x - 1) >> 2) << 2) + 4)
 
 
+// Point to the beginning of the heap
+void *base = NULL;
+
+// Find block that has greater or equal size as requested size
+// Return NULL if no qualified block are present
+static block_ptr find_block(block_ptr *last, size_t size) {
+    block_ptr b = base;
+    while (b && !(b->free && b->size >= size)) {
+        *last = b;
+        b = b->next;
+    }
+    return b;
+}
+
 void *my_malloc_v1(size_t size) {
     void *p;
 
